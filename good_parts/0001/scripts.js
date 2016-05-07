174cb76ad72bbf27e3df55d8784e5960e8da2ace
SotaSuzuki <3.3sota.suzuki@gmail.com>
2016-05-07 21:37:01 +0900

    update

---
+++ good_parts/0001/scripts.js
@@ -0,0 +1,8 @@
+document.writeln('Hello, World!');
+
+
+// 新しいメソッドを定義する際に、本書では以下のようなmethodメソッドを使っている。
+// Function.prototype.method = function (name, func) {
+//     this.prototype[name] = func;
+//     return this;
+// }
